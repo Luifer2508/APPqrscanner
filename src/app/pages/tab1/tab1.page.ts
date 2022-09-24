221f8f0d607929a977513081837df480667f610a
Luis Corrales <Luiscorrales2508@gmail.com>
2022-09-24 00:15:25 -0300

    avance qr

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,78 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { AlertController, Platform } from '@ionic/angular';
+
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements AfterViewInit {
+
+  scanActive:boolean = false;
+
+  constructor(private alertCtrl:AlertController, private platform:Platform) {}
+
+  ngAfterViewInit(): void {
+    if(this.platform.is('capacitor')){
+      BarcodeScanner.prepare();
+    }
+  }
+
+  async scan(){
+    if(this.platform.is('capacitor')){
+      console.log("Comenzando a escanear");
+      const allowed = await this.checkPermisos();
+      if (allowed){
+        this.scanActive = true;
+        const result = await BarcodeScanner.startScan();
+        if (result.hasContent) {
+          console.log(result.content);
+          this.scanActive = false;
+        }
+      }
+    }else {
+      console.log("Corriendo en web")
+    }
+    
+  }
+
+  checkPermisos(){
+    return new Promise(async (resolve, reject)=>{
+      const status = await BarcodeScanner.checkPermission({force:true});
+      if(status.granted)
+      {
+        resolve(true);
+      }
+      else if(status.denied)
+      {
+        const alert = await this.alertCtrl.create({
+          header:'Sin permisos',
+          message:'Por favor permita el acceso a la camara en sus preferencias',
+          buttons: [
+            {
+              text: 'No',
+              role: 'Cancel'
+            },
+            {
+              text: 'Abrir preferencias',
+              handler:()=>{
+                BarcodeScanner.openAppSettings(),
+                resolve(false)
+              }
+            }
+          ]
+        });
+
+        await alert.present();
+      }
+      else
+      {
+        resolve(reject);
+      }
+    })
+  }
+
+}
